1e8cff4aca70b88558d87210efa3cabbb1513dd3
Deeya99902 <deeyaphuyal13@gmail.com>
2025-07-14 17:03:56 +0545

    Worked on Urls and other setups

--- guff/urls.py
+++ guff/urls.py
@@ -1,5 +1,19 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
 
-urlspattern=[
+urlpatterns = [
     
+    # home page
+    path('', views.home, name= 'home'),
+    
+    # Authentication URLS 
+    path('register/', views.register, name= 'register'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name= 'logout'),
+    
+    # Post URLs
+    path('post/create/', views.post_create, name = 'post_create'),
+    path('post/int:post_id/edit/', views.post_edit, name='post_edit'),
+    path('post/int:post_id/delete/', views.post_delete, name='post_delete'),
 ]
